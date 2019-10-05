db7733520b46ac6fcf3c1a78083345ef8d64ef85
RPG3D <imxqliu@gmail.com>
2019-10-05 19:41:05 +0800

    1.fix error

--- BuildAndroid.bat
+++ BuildAndroid.bat
@@ -13,9 +13,11 @@ if not exist "%~dp0%NDKBuild" (
 	mkdir NDKBuild
 )
 
-cd NDKBuild
 
 set PB_SOURCE=%~dp0
+
+cd NDKBuild
+
 
 cmake -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE=%NDK_ROOT%/build/cmake/android.toolchain.cmake -DCMAKE_MAKE_PROGRAM=%NDK_ROOT%/prebuilt/windows-x86_64/bin/make.exe -DANDROID_STL=gnustl_shared   %PB_SOURCE%
 
