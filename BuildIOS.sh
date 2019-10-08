b2d020b39996e11d312e3971b7d525bec4750996
RPG3D <imxqliu@gmail.com>
2019-10-08 20:05:31 +0800

    1.use dll onWin64

--- BuildIOS.sh
+++ BuildIOS.sh
@@ -5,6 +5,6 @@ fi
 
 cd IOSBuild
 
-sudo cmake  .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=../ios.toolchain.cmake -DPLATFORM=OS64
+sudo cmake  .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -DPLATFORM=OS64
 
 sudo cmake --build . -t libprotobuf-lite --config Release
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # ProtobufLite
 test auto compile ProtobufLite on Github Action
 compile Win64&Android on Windows; Mac&iOS on Mac
+use Dll on Win64
